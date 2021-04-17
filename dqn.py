62162f9d403a78a2b11c24ed1b472bd4eef02aff
andrebeu <aobeukers@gmail.com>
2021-04-17 11:43:32 -0400

    refactored to use namedtuple

--- dqn.py
+++ dqn.py
@@ -1,9 +1,23 @@
+import collections
 import numpy
 import gym
 from collections import namedtuple
 import torch as tr
 import gym
 import numpy as np
+
+# container 
+Experience = namedtuple('Experience',[
+    'tstep','state','action','reward','state_tp1'
+])
+
+
+def unpack_expL(expLoD):
+    """ given list of exp (namedtups)
+    return dict of np.arrs
+    """
+    expDoL = Experience(*zip(*expLoD))._asdict()
+    return {k:np.array(v) for k,v in expDoL.items()}
 
 
 
@@ -17,12 +31,12 @@ class Task():
         # used for default random policy
         self.aspace = self.env.action_space.n
 
-    def sample(self,policy_fn=None):
+    def play(self,policy_fn=None,max_ep_len=1000):
         """ 
         given policy, return trajectory
             pi(s_t) -> a_t
-        """
-        max_ep_len = 1000
+        returns episode_exp = {st:[],at:[],rt:[],spt:[]}
+        """        
         self.env.reset()
         # random policy
         if type(policy_fn)==type(None):
@@ -31,111 +45,63 @@ class Task():
         done = False
         tstep = 0
         env_out = self.env.step(0) # arbitrary a0
-        sp_t,r_t,done,extra = env_out
+        sp_t,rt,done,extra = env_out
         # init ep obj for collecting data
-        episode = Episode()
+        episode = []
         while not done:
             tstep += 1 
             s_t = sp_t
             # sample a_t, observe transition
             a_t = policy_fn(s_t)
             env_out_t = self.env.step(a_t)
+            sp_t,rt,done,extra = env_out_t
             # collect transition 
-            sp_t,r_t,done,extra = env_out_t
-            episode.record_trans(s_t,a_t,r_t,sp_t)
+            episode.append(
+                Experience(tstep,s_t,a_t,rt,sp_t)
+            )
             # verify if done
-            if tstep>=max_ep_len:
+            if tstep==max_ep_len:
                 done=True
         return episode
 
 
-class Episode():
-
-    """ helper object 
-    for recording/manipulating
-    trajectories in episode
-    """
-
-    def __init__(self):
-        self.trajectory = []
-        None
-
-    def record_trans(self,st,at,rt,spt):
-        transition = {
-            'st':st,'at':at,
-            'rt':rt,'spt':spt
-        }
-        self.trajectory.append(transition)
-        return None
-
-    def format_out(self):
-        """ 
-        reformats trajectory in buffer 
-        from list of dicts, to dict of np.arrs
-        """
-        traj = {
-            'st':[],'at':[],
-            'rt':[],'spt':[]
-        }
-        # loop over transitions in trajectory
-        for trans in self.trajectory:
-            traj['st'].append(trans['st'])
-            traj['at'].append(trans['at'])
-            traj['rt'].append(trans['rt'])
-            traj['spt'].append(trans['spt'])
-        # list to nparr
-        traj = {k:np.array(v) for k,v in traj.items()}
-        return traj
-
 
 class Buffer():
-    """ deque with sample method
+    """ deque with record and sample method
     """
 
-    def __init__(self,nsamples=64):
-        import collections
-        self.len = nsamples
-        self.buffer = collections.deque(maxlen=nsamples)
-        None
+    def __init__(self,size=10000):
+        """ buffer is list of dicts
+        """
+        self.buff_size = size
+        self.reset_buff()
+        return None
+
+    def reset_buff(self):
+        self.buffer = collections.deque(maxlen=self.buff_size)
+        return None
 
     def record(self,episode,verb=False):
-        """ 
-        given an episode (or experience more generally)
-        append to bank
+        """ record episode
+        append list of experiences to buffer
         """
-        self.buffer.extend(episode.trajectory)
-        if verb:
-            print('append',self.len)
-        None
+        self.eplen = len(episode)
+        self.buffer.extend(episode)
+        return None
 
-    def sample(self,nsamples=1000):
-        """ sample transitions from the bank
+    def sample(self,mode='rand',nsamples=64):
+        """ sample exp from the buffer
+        return 
         """
-        # sample list of transitions
-        exp = np.random.choice(self.buffer,nsamples)
-        # return dict of array
-        return self.format_out(exp) 
-
-    def format_out(self,experience):
-        """ 
-        experience is list of transitions
-            []
-        formats output as dict of nparrs
-        """
-        exp = {
-            'st':[],'at':[],
-            'rt':[],'spt':[]
-        }
-        # loop over transitions in trajectory
-        for transition in experience:
-            exp['st'].append(transition['st'])
-            exp['at'].append(transition['at'])
-            exp['rt'].append(transition['rt'])
-            exp['spt'].append(transition['spt'])
-        # list to nparr
-        exp = {k:np.array(v) for k,v in exp.items()}
-        return exp
-
+        # consider all samples in buffer
+        if mode == 'rand': 
+            exp = self.buffer
+        # only consider last episode
+        elif mode == 'ep': 
+            exp = list(self.buffer)[-self.eplen:]
+        # return dict of array 
+        exp_samples = np.random.choice(exp,nsamples)
+        return exp_samples
 
 
 
@@ -144,12 +110,12 @@ class DQN(tr.nn.Module):
     def __init__(self):
         super().__init__()
         self.indim = 4 # 4 obs + 1 action
-        self.stsize = 23
+        self.stsize = 48
         self.outdim = 2 # num actions
         self.build()
         self.lossop = tr.nn.SmoothL1Loss()
         self.optiop = tr.optim.RMSprop(self.parameters(),lr=0.001)
-        self.gamma=0.9
+        self.gamma=0.98
 
     def build(self):
         self.ff1 = tr.nn.Linear(self.indim,self.stsize)
@@ -174,35 +140,40 @@ class DQN(tr.nn.Module):
         q_val = self.out_layer(h_t)
         return q_val
 
-    def eval(self,exp):
-        st = exp['st']
-        at = exp['at']
-        r_t = exp['rt']
-        stp1 = exp['spt']
-        None
+    def qlearn_loss(self,exp):
+        """ 
+        exp is dict of arrs
+            {state,action,reward,state_tp1}
+        """
+        st = exp['state']
+        at = exp['action']
+        rt = exp['reward']
+        stp1 = exp['state_tp1']
+        # forward passes
+        q_stp1 = self.forward(stp1)
+        q_st = self.forward(st)
+        ## form ytarget 
+        # max_ap{ q(sp_t,ap_t) }
+        q_stp1_max_ap = tr.max(q_stp1,1)[0]
+        # discount
+        ytarget = tr.Tensor(rt) + self.gamma*q_stp1_max_ap
+        # final target = reward
+        ytarget[-1] = rt[-1] 
+        # yhat = qvalue of selected actions
+        yhat = q_st_at = np.take_along_axis(q_st,at[:,None],axis=1)
+        ## episode loss
+        ep_loss = self.lossop(ytarget,yhat.squeeze())
+        return ep_loss
 
     def train(self,exp):
-        st = exp['st']
-        at = exp['at']
-        r_t = exp['rt']
-        stp1 = exp['spt']
-        ## form ytarget 
-        # max_ap{q(s_tp1,ap_t)}
-        q_stp1 = self.forward(stp1)
-        q_stp1_max_ap = tr.max(q_stp1,1)[0]
-        # form ytarget
-        y_t = tr.Tensor(r_t) + self.gamma*q_stp1_max_ap
-        y_t[-1] = r_t[-1]
-        ## predict q(s_t,a_t)
-        q_st = self.forward(st)
-        # NB: at[:,None] unsqueezes None dim
-        q_st_at = np.take_along_axis(q_st,at[:,None],axis=1)
-        ## episode loss
-        ep_loss = self.lossop(y_t,q_st_at.squeeze())
-        # optimizer step
+        """ 
+        exp is dict of arrs
+            {state,action,reward,state_tp1}
+        """
         self.optiop.zero_grad()
-        ep_loss.backward()
+        loss = self.qlearn_loss(exp)
+        loss.backward()
         self.optiop.step()
-        return np.sum(r_t)
+        return None
 
 
